58a7988ec432f044477cc27027ec0e02d4b3db8b
tonit <toni@okidokiteam.com>
2011-01-09 10:56:37 +0100

    javadoc

--- pax-repository-api/src/main/java/org/ops4j/base/io/InputStreamSource.java
+++ pax-repository-api/src/main/java/org/ops4j/base/io/InputStreamSource.java
@@ -1,35 +1,1 @@
-/*
- * Copyright (C) 2010 Toni Menzel
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.ops4j.base.io;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-/**
- * Indirection for reading from an input stream.
- * Perhaps add metadata because we really have a resource here, not just "search for some GAV" thing.
- */
-public interface InputStreamSource
-{
-
-    /**
-     * @return the concrete, ready to use, stream.
-     *
-     * @throws java.io.IOException in case of a problem initializing the stream.
-     */
-    InputStream get()
-        throws IOException;
-}
+/* * Copyright (C) 2010 Toni Menzel * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *      http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.base.io;import java.io.IOException;import java.io.InputStream;/** * Indirection for reading from an input stream. * Implementations usually should support re-getting (re-openeing) the stream. * Thats the real benefit over raw inputstreams. But its not always possible. */public interface InputStreamSource{    /**     * @return the concrete, ready to use, stream.     *     * @throws java.io.IOException in case of a problem initializing the stream.     */    InputStream get()        throws IOException;}
