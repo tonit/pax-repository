0bad1d0697061030140c94fcd00c9b2da6a38d14
tonit <toni@okidokiteam.com>
2011-01-11 06:59:28 +0100

    a lot of refactoring, introduced Index type to make Resolvers independent of the underlying type system (maven or not..)

--- pax-repository-api/src/main/java/org/ops4j/pax/repository/Resolver.java
+++ pax-repository-api/src/main/java/org/ops4j/pax/repository/Resolver.java
@@ -1,35 +1,1 @@
-/*
- * Copyright (C) 2010 Toni Menzel
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.ops4j.pax.repository;
-
-/**
- * Access to a repository. Gracefully handles incomplete identifiers.. like missing version for example
- */
-public interface Resolver<T extends ArtifactQuery>
-{
-
-    /**
-     * Map an query to concrete resource.
-     *
-     * @param query what to find
-     *
-     * @return An artifact
-     *
-     * @throws RepositoryException in case of a problem.
-     */
-    Artifact find( T query )
-        throws RepositoryException;
-}
+/* * Copyright (C) 2010 Toni Menzel * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *      http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.pax.repository;/** * Access to a repository. Gracefully handles incomplete identifiers.. like missing version for example * TODO: this is more an "index" than an actual resolver. Basically you ask someone who has done the resolve upfront. */public interface Resolver<T extends ArtifactQuery>{    /**     * Map an query to concrete resource.     *     * @param query what to find     *     * @return An artifact     *     * @throws RepositoryException in case of a problem.     */    Artifact find( T query )        throws RepositoryException;}
--- pax-repository-maven-api/src/main/java/org/ops4j/pax/repository/maven/GavArtifactQueryParser.java
+++ pax-repository-maven-api/src/main/java/org/ops4j/pax/repository/maven/GavArtifactQueryParser.java
@@ -1,53 +1,1 @@
-/*
- * Copyright (C) 2010 Toni Menzel
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.ops4j.pax.repository.maven;
-
-import org.ops4j.pax.repository.ArtifactQuery;
-import org.ops4j.pax.repository.spi.Parser;
-
-/**
- * Parses a string of format: GROUP:ARTIFACT:VERSION[:classifier] to an identifier.
- */
-public class GavArtifactQueryParser implements Parser<ArtifactQuery, GavArtifactQuery>
-{
-
-    public GavArtifactQuery parse( ArtifactQuery q )
-    {
-
-        String path = q.getQueryString();
-        String paths[] = path.split( ":" );
-        if( path.length() > 2 )
-        {
-            String groupId = paths[ 0 ];
-            String artifactId = paths[ 1 ];
-            String versionId = paths[ 2 ];
-            String classifier = null;
-
-            if( paths.length > 3 )
-            {
-                classifier = paths[ 3 ];
-            }
-
-            return new DefaultArtifactQuery( groupId, artifactId, versionId, classifier );
-        }
-        else
-        {
-            throw new IllegalArgumentException( "Cannot parse \"" + path + "\" as <GROUP>:<ARTIFACT>:<VERSION>[:CLASSIFIER]." );
-        }
-
-
-    }
-}
+/* * Copyright (C) 2010 Toni Menzel * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *      http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.pax.repository.maven;import org.ops4j.pax.repository.ArtifactQuery;import org.ops4j.pax.repository.spi.Parser;/** * Parses a string of format: GROUP:ARTIFACT:VERSION[:classifier] to an identifier. */public class GavArtifactQueryParser implements Parser<ArtifactQuery, GAV>{    public GAV parse( ArtifactQuery q )    {        String path = q.getQueryString();        String paths[] = path.split( ":" );        if( path.length() > 2 )        {            String groupId = paths[ 0 ];            String artifactId = paths[ 1 ];            String versionId = paths[ 2 ];            String classifier = null;            if( paths.length > 3 )            {                classifier = paths[ 3 ];            }            return new DefaultArtifactQuery( groupId, artifactId, versionId, classifier );        }        else        {            throw new IllegalArgumentException( "Cannot parse \"" + path + "\" as <GROUP>:<ARTIFACT>:<VERSION>[:CLASSIFIER]." );        }    }}
