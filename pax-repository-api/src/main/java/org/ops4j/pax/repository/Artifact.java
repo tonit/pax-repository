25a4f00e5a78f24db4d0f515cfd5ccddda42c046
tonit <toni@okidokiteam.com>
2011-02-01 21:52:01 +0100

    Refactored InputStreamSource to Source<T>

--- pax-repository-api/src/main/java/org/ops4j/pax/repository/Artifact.java
+++ pax-repository-api/src/main/java/org/ops4j/pax/repository/Artifact.java
@@ -1,36 +1,1 @@
-/*
- * Copyright (C) 2010 Toni Menzel
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.ops4j.pax.repository;
-
-import org.ops4j.base.io.InputStreamSource;
-
-/**
- * A resolved artifact. You can get the resource
- */
-public interface Artifact
-{
-
-    /**
-     * Raw data of this artifact as a delayed InputStream.
-     *
-     * @return access to the raw data.
-     *
-     * @throws RepositoryException problems ?
-     */
-    InputStreamSource getContent()
-        throws RepositoryException;
-
-}
+/* * Copyright (C) 2010 Toni Menzel * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *      http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.pax.repository;import org.ops4j.base.io.Source;/** * A resolved artifact. You can get the resource */public interface Artifact<T>{    /**     * Raw data of this artifact as a delayed Source.     *     * @return access to the raw data.     *     * @throws RepositoryException problems ?     */    Source<T> getContent() throws RepositoryException;}
--- pax-repository-base/src/main/java/org/ops4j/pax/repository/base/CachedArtifact.java
+++ pax-repository-base/src/main/java/org/ops4j/pax/repository/base/CachedArtifact.java
@@ -1,56 +1,1 @@
-/*
- * Copyright (C) 2010 Toni Menzel
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.ops4j.pax.repository.base;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.ops4j.base.io.InputStreamSource;
-import org.ops4j.pax.repository.Artifact;
-import org.ops4j.store.Handle;
-import org.ops4j.store.Store;
-
-/**
- * Convinience, OPS4J-Base-Store based Artifact implementation.
- */
-public class CachedArtifact implements Artifact
-{
-
-    private final Store<InputStream> m_store;
-    private final Handle m_handle;
-    
-    public CachedArtifact( Store<InputStream> store, InputStream inp )
-        throws IOException
-    {
-        m_store = store;
-        m_handle = store.store( inp );
-    }
-
-    public InputStreamSource getContent()
-    {
-        return new InputStreamSource()
-        {
-
-            public InputStream get()
-                throws IOException
-            {
-                return m_store.load( m_handle );
-            }
-        };
-    }
-
-
-}
+/* * Copyright (C) 2010 Toni Menzel * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *      http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.pax.repository.base;import java.io.IOException;import org.ops4j.base.io.Source;import org.ops4j.pax.repository.Artifact;import org.ops4j.store.Handle;import org.ops4j.store.Store;/** * Convinience, OPS4J-Base-Store based Artifact implementation. */public class CachedArtifact<T> implements Artifact<T>{    private final Store<T> m_store;    private final Handle m_handle;        public CachedArtifact( Store<T> store, T inp )        throws IOException    {        m_store = store;        m_handle = store.store( inp );    }    public Source getContent()    {        return new Source<T>()        {            public T get()                throws IOException            {                return m_store.load( m_handle );            }        };    }}
