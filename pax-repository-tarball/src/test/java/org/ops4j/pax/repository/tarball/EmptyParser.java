a2c773e557fd2664bf35d9f542080bddcbf86529
tonit <toni@okidokiteam.com>
2011-02-01 21:34:51 +0100

    consolidate api into one single package

--- pax-repository-tarball/src/test/java/org/ops4j/pax/repository/tarball/EmptyParser.java
+++ pax-repository-tarball/src/test/java/org/ops4j/pax/repository/tarball/EmptyParser.java
@@ -1,1 +1,1 @@
-package org.ops4j.pax.repository.tarball;import org.ops4j.pax.repository.spi.Parser;/** * Do nothing parser. Just returns the imput instance. */public class EmptyParser<T> implements Parser<T, T>{    public T parse( T source )    {        return source;    }}
+package org.ops4j.pax.repository.tarball;import org.ops4j.pax.repository.Parser;/** * Do nothing parser. Just returns the imput instance. */public class EmptyParser<T> implements Parser<T, T>{    public T parse( T source )    {        return source;    }}
